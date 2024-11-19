a4e7833261b50770b36cbc3a128b0d725c015098
Deepak Sharma <ds9999106554@gmail.com>
2024-11-20 01:12:07 +0530

    modifi

--- EyeRefer_server/src/index.ts
+++ EyeRefer_server/src/index.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config({path:`./src/config/.env.${process.env.NODE_ENV}`});
-console.log(process.env.NODE_ENV)
 
 import express from "express";
 import cors from "cors"
