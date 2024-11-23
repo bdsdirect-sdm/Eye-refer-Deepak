3c8b3a03d2ee2cfbc6d759cb56efad2c181a0d26
BD sDirect <bdsdirect.sdm@smartdatainc.net>
2024-11-23 05:31:16 +0530

    06:31

--- EyeRefer_server/src/services/doctor.service.ts
+++ EyeRefer_server/src/services/doctor.service.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
 import { constantValues } from "../constants";
 import { ReturnResult } from "../interfaces/returnResultInterface";
+import AppError from "../utils/errorHandler";
 
 const doctorSignUp = async (req:Request):Promise<ReturnResult> =>{
     const  {name, email} = req.body;
+    if(! name || !email){
+        throw new AppError(constantValues.msg.passwordChangeEmail,constantValues.msgCode.conflictCode)
+        
+    }
     const result : ReturnResult =  {
         success:constantValues.msgType.successStatus,
         message:constantValues.msg.doctorAdd,
